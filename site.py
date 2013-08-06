b6fde39e65ffd1a5c516f2bc8080095e24422159
Ali Nahm <alisnahm@gmail.com>
2013-08-06 02:02:36 -0700

    adding more to the site.py for form post data dump

--- site.py
+++ site.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import jsonify, render_template, request
 app = Flask(__name__)
 
 from flask.ext.wtf import Form, TextField, Required
@@ -14,11 +15,24 @@ def chatvisForm():
     otherName = TextField('otherName', validators = [Required()])
     searchText = TextField('searchText', validators = [Required()])
 
+@app.route('/chatvisForm')
+def chatvisFormProcess():
+    otherName = request.values.get('otherName')
+    searchText = request.values.get('searchText')
+
+    # now to do the work...
+    convo = getAliFile(otherName)
+    if (convo == None):
+        return jsonify(convoLen = 0, strCounter = 0)
+    numStr = phraseCount(convo, searchText)
+
+    return jsonify(convoLen = len(convo), strCounter = numStr)
+
 @app.route('/chatvis')
 def chatvis():
     form = chatvisForm()
-    render_template('chatvis.html',
-            form = form)
+    if (form):
+        render_template('chatvis.html', form = form)
 
 if __name__ == '__main__':
     app.debug = True
